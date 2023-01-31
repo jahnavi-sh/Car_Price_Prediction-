46818f17e11b3091d5a25025f606319a3fab95f6
jahnavi-sh <114072219+jahnavi-sh@users.noreply.github.com>
2023-01-31 20:34:19 +0530

    Update car_price_prediction.py

--- car_price_prediction.py
+++ car_price_prediction.py
@@ -27,7 +27,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 
 #model development and evaluation 
-from sklearn.metrics import train_test_split 
+from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
 from sklearn import metrics
